8c041509508749b8a576784b0b9f57eeb78e9edb
Jonathas Guilherme <jonathasguilhermeti@gmail.com>
2025-12-09 00:39:05 -0300

    Adicionando Math.ceil e Math.floor

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 
 
 function sortear () {
-    const min = parseInt(document.querySelector('.input-min').value);
-    const max = parseInt(document.querySelector('.input-max').value);
+    const min = Math.ceil(document.querySelector('.input-min').value);
+    const max = Math.floor(document.querySelector('.input-max').value);
     const sorteado = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(sorteado);
 }
