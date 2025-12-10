df4c10e6f4e75b5b39970394364a40c48af2cc06
Jonathas Guilherme <jonathasguilhermeti@gmail.com>
2025-12-10 00:49:37 -0300

    Adicionando operadores de comparação nos valores min e max

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@
 function sortear () {
     const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
-    const sorteado = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(sorteado);
+
+    if (max > min) {
+        const sorteado = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(sorteado)
+    }else {
+        alert('O valor mínimo deve ser menor que o máximo.')
+    }
 }
